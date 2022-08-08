6260e5e588e44a7d23216ebdaf9a6f65e7541de9
Jarrod Medrano <jmedran@gmail.com>
2022-08-07 21:53:12 -0600

    strings and strings slices

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,18 @@ fn main() {
     let (name, latitude, longitude) = location2;
     println!("location name: {}, latitude: {}, longitude: {}", name, latitude, longitude);
     let location: [f32;2] = [0.0, 0.0];
+
+
+    //string slice
+    let person_name_slice = "Donald Mallard";
+
+    //string stored on the heap
+    let person_name_string2 = String::from("Donald Mallard").to_string();
+    let person_name_string3 = "donald mallard".to_string();
+
+    println!("{}", person_name_slice);
+    println!("{}", person_name_string2);
+    println!("{}", person_name_string3);
 }
 
 
